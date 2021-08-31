335182db5fca65d2d474f2d20fede02e952c9b39
jcnghm <jcnghmpsEgmail.com>
2021-08-31 13:59:16 -0700

    first commit

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Give access to the project in ANY OS we find ourselves in
+# Allow outside files/folders to be added to the project from the base dir
+
+
+class Config():
+    """
+        Set Config variables for the flask app.
+        Using Environment variables where available otherwise
+        create the config variables if not done already.
+    """
+
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will never guess'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:PASSWORD HERE@127.0.0.1:5432/drone-collection' # Need to enter server password
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # Turn off update messages from the sqlalchemy
+
+
+# This is for database URI using url temporarily:   
+# os.environ.get('DATABSE_URL') or 'sqlite:///' + os.path.join(basedir,'app.db')
